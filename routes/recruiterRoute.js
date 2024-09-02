9377873fcf751f63d909e4416491d111d601020e
Ak4shKr <akashkmr7667@gmail.com>
2024-09-02 21:49:16 +0530

    added some more features at reacruiter side

--- routes/recruiterRoute.js
+++ routes/recruiterRoute.js
@@ -2,9 +2,15 @@ import express from "express";
 import {
   recruiterRegister,
   recruiterLogin,
+  jobpost,
+  alljobpostbyrecruiter,
+  deletejob,
 } from "../controllers/recruiterController.js";
 const router = express.Router();
 
 router.post("/register", recruiterRegister);
 router.post("/login", recruiterLogin);
+router.post("/jobpost", jobpost);
+router.post("alljobpostbyrecruiter", alljobpostbyrecruiter);
+router.delete("deletejob/:id", deletejob);
 export default router;
