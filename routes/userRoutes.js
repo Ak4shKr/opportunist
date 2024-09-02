26ba8790e791f7d29106827c973fdb263f89e10a
Ak4shKr <akashkmr7667@gmail.com>
2024-09-02 21:48:57 +0530

    added routes for application and contact features

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { userRegister, userLogin } from "../controllers/userController.js";
+import {
+  userRegister,
+  userLogin,
+  userApplication,
+  contactQuery,
+} from "../controllers/userController.js";
+import { sendEmail } from "../services/emailService.js";
 const router = express.Router();
 
 router.post("/register", userRegister);
 router.post("/login", userLogin);
+router.post("/application", userApplication);
+router.post("/contact", contactQuery);
+
 
 export default router;
