f9e8147ba550aaeda365d418ab2af9024d9ffb63
Ak4shKr <akashkmr7667@gmail.com>
2024-09-02 21:51:06 +0530

    worked upon user and recruiter contrller for developed routes

--- controllers/recruiterController.js
+++ controllers/recruiterController.js
@@ -1,3 +1,5 @@
+import ApplyByJob from "../models/ApplyByJobModel.js";
+import Job from "../models/JobModel.js";
 import Recruiter from "../models/RecruiterModel.js";
 import jwt from "jsonwebtoken";
 export const recruiterRegister = async (req, res) => {
@@ -52,6 +54,111 @@ export const recruiterLogin = async (req, res) => {
       recruiter: recruiter,
       token,
     });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const jobpost = async (req, res) => {
+  try {
+    const {
+      title,
+      salary,
+      eligibility,
+      location,
+      noticePeriod,
+      experience,
+      skills,
+      description,
+      jobType,
+      recruiterId,
+      jobfile,
+    } = req.body;
+
+    if (
+      !title ||
+      !salary ||
+      !eligibility ||
+      !location ||
+      !noticePeriod ||
+      !experience ||
+      !skills ||
+      !description ||
+      !jobType
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const job = new Job({
+      title,
+      salary,
+      eligibility,
+      location,
+      noticePeriod,
+      experience,
+      skills,
+      description,
+      jobType,
+      recruiterId,
+      jobfile,
+    });
+    await job.save();
+    res.status(201).json({ message: "Job posted successfully" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const alljobpostbyrecruiter = async () => {
+  try {
+    const recruiterId = req.body;
+    const jobs = await Job.find({ recruiterId });
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const deletejob = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId);
+    if (job) {
+      job.status = "inactive";
+      await job.save();
+    } else {
+      return res.status(404).json({ message: "Job not found" });
+    }
+    res.status(200).json({ message: "Job deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const viewApplicantsByJob = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId);
+    if (job) {
+      const applicants = await Application.find({ jobId });
+      res.status(200).json(applicants);
+    } else {
+      return res.status(404).json({ message: "Job not found" });
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const shortlist = async () => {
+  try {
+    const { jobId, applicationId } = req.params.id;
+    const applyByJob = await ApplyByJob.findOne({ jobId, applicationId });
+    if (applyByJob) {
+      applyByJob.status = "shortlisted";
+      await applyByJob.save();
+    } else {
+      return res.status(404).json({ message: "Application not found" });
+    }
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
+import Application from "../models/ApplicationModel.js";
+import Contact from "../models/ContactModel.js";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
+import { sendEmail } from "../services/emailService.js";
 
 export const userRegister = async (req, res) => {
   try {
@@ -47,6 +50,88 @@ export const userLogin = async (req, res) => {
     res
       .status(200)
       .json({ message: "Login Successful as User!", token, user: user });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const userApplication = async (req, res) => {
+  try {
+    const {
+      name,
+      experiance,
+      currentCompany,
+      currentDesignation,
+      noticePeriod,
+      skills,
+      email,
+      mobile,
+      linkedin,
+      github,
+      resume,
+      coverLetter,
+    } = req.body;
+    if (
+      !name ||
+      !experiance ||
+      !currentCompany ||
+      !currentDesignation ||
+      !noticePeriod ||
+      !skills ||
+      !email ||
+      !mobile ||
+      !linkedin ||
+      !github ||
+      !resume ||
+      !coverLetter
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const application = new Application({
+      name,
+      experiance,
+      currentCompany,
+      currentDesignation,
+      noticePeriod,
+      skills,
+      email,
+      mobile,
+      linkedin,
+      github,
+      resume,
+      coverLetter,
+    });
+    await application.save();
+    res
+      .status(201)
+      .json({ message: "Badhai Ho 😎, aapka application submit ho gya hai" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const contactQuery = async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    if (!name || !email || !message) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const contact = new Contact({
+      name,
+      email,
+      message,
+    });
+    await contact.save();
+    // const sendEmailres = await sendEmail({
+    //   email: "21001008006@jcboseust.ac.in",
+    //   subject: "OTP for password reset",
+    //   text: `Your OTP is 5555, for your password reset. it is valid for only 2 minute.
+    //   \nTeam: CatchCorruption`,
+    // });
+    // console.log(sendEmailres);
+    res
+      .status(201)
+      .json({ message: "Badhai Ho 😎, aapki baatein ham tak pahuch gyi hai!" });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
